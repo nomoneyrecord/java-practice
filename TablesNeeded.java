f8f67e4c2a7f85d52931327fc7a862edb5dfd589
Gary H <3rdeye.ghj@gmail.com>
2025-03-21 10:10:51 -0700

    writes TablesNeeded class and calculation practice

---
+++ TablesNeeded.java
@@ -0,0 +1,13 @@
+import java.util.Scanner; 
+
+public class TablesNeeded {
+  public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+
+      int tableSize = 10;
+      int guests = scnr.nextInt();
+      int tablesFilled = guests / tableSize; 
+
+      System
+  }
+}
