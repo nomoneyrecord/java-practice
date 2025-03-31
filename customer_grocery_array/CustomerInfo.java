8febd5f4966d248912c1f5dad9a816f196947004
Gary H <3rdeye.ghj@gmail.com>
2025-03-30 17:14:59 -0700

    implements the execution logic for CustomerInfo.java and relevant constructors, methods, gets, and set

---
+++ customer_grocery_array/CustomerInfo.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class CustomerInfo {
+  public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in); 
+
+      String name, item;
+      double price, total = 0;
+      int quantity; 
+
+      Grocery grocery = null;
+      Customer customer1 = new Customer();
+      name = scnr.next();
+
+      String sentinel = "y";
+
+      do { 
+        item = scnr.next();
+        price = scnr.nextDouble();
+        quantity = scnr.nextInt();
+
+        grocery = new Grocery(item, price, quantity);
+        customer1.addGrocery(grocery);    
+      } while (sentinel.equals("y")); {
+      
+        System.out.println(name + " has the following items:\n");
+
+        for(Grocery g : customer1.getGroceryList()){
+          System.out.println("Item: " + g.getItem());
+          System.out.println("Price: " + g.getPrice());
+          System.out.println("Quantity: " + g.getQuantity());
+          total += g.getPrice();
+        }
+
+        System.out.println("Total: " + total);
+      }
+  }
+}
