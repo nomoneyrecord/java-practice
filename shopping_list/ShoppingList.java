2bdeb77880cb9ed4fc0489c5a274f2e4c1c0dff7
Gary H <3rdeye.ghj@gmail.com>
2025-05-08 13:39:38 -0700

    adds shopping_list folder with ListItem.java and ShoppingList.java files and lab boilerplates with solutions

---
+++ shopping_list/ShoppingList.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ShoppingList {
+   public static void main (String[] args) {
+      Scanner scnr = new Scanner(System.in);
+
+      // TODO: Declare a LinkedList called shoppingList of type ListItem
+
+      String item;
+      
+      // TODO: Scan inputs (items) and add them to the shoppingList LinkedList
+      //       Read inputs until a -1 is input
+     
+      
+      // TODO: Print the shoppingList LinkedList using the printNodeData() method
+    
+
+   }
+}
